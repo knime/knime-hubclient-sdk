7484fa4970888c382659ad60b1f7f5d7d785a982
Magnus Gohm <magnus.gohm@knime.com>
2026-01-29 13:16:19 +0100

    AP-25576: Added TeamAccount entity and deserilization tests
    AP-25576 (Adjust space root fetching to include team accounts)

---
+++ org.knime.hub.client/src/org/knime/hub/client/sdk/ent/account/TeamAccount.java
@@ -0,0 +1,127 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Jan 29, 2026 (magnus): created
+ */
+package org.knime.hub.client.sdk.ent.account;
+
+import java.util.Objects;
+
+import org.knime.hub.client.sdk.ent.util.EntityUtil;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * POJO representing the team account identity.
+ *
+ * @author Magnus Gohm, KNIME AG, Konstanz, Germany
+ * @since 1.1
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public final class TeamAccount extends AccountIdentity {
+
+    static final String TYPE = "TEAM";
+
+    private static final String JSON_PROPERTY_NAME = "name";
+    private final String m_name;
+
+    /**
+     * Team account identity.
+     *
+     * @param id the ID of the team account
+     * @param name the name of the team
+     */
+    @JsonCreator
+    public TeamAccount(@JsonProperty(value = AccountIdentity.JSON_PROPERTY_ID, required = true) final String id,
+        @JsonProperty(value = TeamAccount.JSON_PROPERTY_NAME, required = true) final String name) {
+        super(id);
+        m_name = name;
+    }
+
+    /**
+     * Retrieves the name of the user account.
+     *
+     * @return name
+     */
+    @JsonProperty(JSON_PROPERTY_NAME)
+    @JsonInclude(value = JsonInclude.Include.ALWAYS)
+    public String getName() {
+        return m_name;
+    }
+
+    @Override
+    public AccountIdentityType getType() {
+        return AccountIdentityType.TEAM;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        var teamAccount = (TeamAccount) o;
+        return Objects.equals(this.getId(), teamAccount.getId())
+                && Objects.equals(this.getType(), teamAccount.getType())
+                && Objects.equals(this.m_name, teamAccount.m_name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId(), this.getType(), m_name);
+    }
+
+    @Override
+    public String toString() {
+        return EntityUtil.toString(this);
+    }
+
+}
